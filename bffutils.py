36598ac4806d8a9f57e0a0f92b03b619f8b49fa0
Steffen <kresselsteffen@gmail.com>
2022-02-03 20:35:32 +0100

    first commit

---
+++ bffutils.py
@@ -0,0 +1,38 @@
+import os
+from tkinter import filedialog
+
+def add_path(parentwidget, listname ,paths):
+    x = filedialog.askdirectory()
+    paths.append(x)
+    list = parentwidget.children[listname]
+    list.insert(0, x)
+    save_paths(paths)
+
+def load_paths():
+    pathlist = []
+    if os.path.isfile("paths.txt"):
+        savefile = open("paths.txt", "r")
+        for path in savefile.readlines():
+            pathlist.append(path)
+        savefile.close()
+    else:
+        savefile = open("paths.txt", "w")
+        print("file not found, created one")
+    return pathlist
+
+def save_paths(paths):
+    print("saving: ", paths)
+    savefile = open("paths.txt", "w")
+    for i in paths:
+        savefile.write(i + "\n") 
+    savefile.close()
+
+def delete_path(parentwidget, listname, paths):
+    list = parentwidget.children[listname]
+    try:
+        delete_index = list.curselection()[0]
+        list.delete(delete_index)
+        paths.pop(-delete_index-1)
+        save_paths(paths)
+    except IndexError:
+        print("no path selected")
